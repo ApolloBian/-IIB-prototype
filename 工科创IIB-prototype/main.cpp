24f5da2393502db3417fdac957466f2c29500fe7
Apollo <apollo@apomarki.local>
2015-11-03 19:27:00 +0800

    Initial Commit

---
+++ 工科创IIB-prototype/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  工科创IIB-prototype
+//
+//  Created by Apollo on 11/3/15.
+//  Copyright © 2015 Apollo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

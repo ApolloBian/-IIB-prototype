330549094257aee4a3d4eabd2bcc07e711b65d9b
Apollo <apollo@apomarki.local>
2015-11-03 19:30:50 +0800

    get started

--- 工科创IIB-prototype/main.cpp
+++ 工科创IIB-prototype/main.cpp
@@ -1,15 +1,1 @@
-//
-//  main.cpp
-//  工科创IIB-prototype
-//
-//  Created by Apollo on 11/3/15.
-//  Copyright © 2015 Apollo. All rights reserved.
-//
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+// main control function for PCCC
